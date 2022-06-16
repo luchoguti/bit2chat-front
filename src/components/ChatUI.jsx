15b842ba50d5bf8fd9e9ed2f8d0bf36b743e8e4e
Luis Alberto Torres Gutierrez <luchoguti9@gmail.com>
2022-06-16 11:17:22 -0500

    It make the test for enterprise bit2chat developer position front-end

--- src/components/ChatUI.jsx
+++ src/components/ChatUI.jsx
@@ -1,17 +1,49 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 
 import ChatContext from "../context/Chat/ChatContext";
+import {mutateDataChat} from "../utils/utilsFunctions";
 
 const ChatUI = () => {
     const {
         chatQueryResponse
     } = useContext(ChatContext);
+    const [conversation, setConversation] = useState([]);
     useEffect(() => {
-        console.log(chatQueryResponse);
-    },[chatQueryResponse]);
+        if (typeof chatQueryResponse.id !== "undefined") {
+            let chat_conv = mutateDataChat(chatQueryResponse);
+            console.log(chat_conv);
+            setConversation(chat_conv);
+        }
+    }, [chatQueryResponse]);
     return (
         <>
-            luis torres
+            <div className="menu">
+                <div className="back">
+                    <i className="fa fa-chevron-left"></i>
+                    <img src="https://www.pngitem.com/pimgs/m/488-4881316_female-chat-support-png-image-call-center-agent.png" draggable="false"/>
+                </div>
+                <div className="name">Agent Chat</div>
+                <div className="last">12:00</div>
+            </div>
+            <ol className="chat">
+                {
+                    conversation.map((conv,key)=>{
+                      return (
+                          <li className={conv.type == "INCOMING_MESSAGE"?"other":"self"} key={key}>
+                              <div className="msg">
+                                  <p>
+                                      {
+                                          conv.text
+                                      }
+                                  </p>
+                                  <time>{conv.date}</time>
+                              </div>
+                          </li>
+                      )
+                    })
+                }
+            </ol>
+            <input className="textarea" type="text" placeholder="Message"/>
         </>
     )
 }
--- src/utils/utilsFunctions.jsx
+++ src/utils/utilsFunctions.jsx
@@ -1,6 +1,17 @@
-import ConversationInterface from '../interfaces/conversation.interface';
-const mutateDataChat = (data: ConversationInterface) => {
-    data.events.map((value,key)=>{
-        
-    })
+export const mutateDataChat = (data) => {
+    return data.events.map((data_conversation)=>{
+        let mutate = {};
+        mutate['id'] = data_conversation.payload.messageId;
+        if(data_conversation.type === "INCOMING_MESSAGE"){
+            mutate['fullName'] = data.contact.fullName;
+        }else{
+            mutate['fullName'] = data.assignedTo.fullName;
+        }
+        mutate['text'] = data_conversation.payload.text;
+        mutate['type'] = data_conversation.type;
+        mutate['date'] = new Date(data_conversation.payload.sendingTime).toLocaleTimeString('en-US',
+            {timeZone:'UTC',hour12:true,hour:'numeric',minute:'numeric'}
+        );
+        return mutate;
+    });
 }
